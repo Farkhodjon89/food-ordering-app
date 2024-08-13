c3b0601389c46c7436d59f9a1a44a58cdf579289
Farhod <farhod@MacBook-Air-Farhod.local>
2024-08-13 16:38:57 +0500

    d

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,9 +1,9 @@
-import { User } from "@/models/User"
-import mongoose from "mongoose"
+// import { User } from "@/models/User"
+// import mongoose from "mongoose"
 
-export async function GET() {
-    mongoose.connect(process.env.MONGO_URL)
-    return Response.json(
-        await User.find()
-    )
-}
+// export async function GET() {
+//     mongoose.connect(process.env.MONGO_URL)
+//     return Response.json(
+//         await User.find()
+//     )
+// }
